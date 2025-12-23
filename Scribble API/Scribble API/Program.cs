e08edc88d584e5fb8c5903f1b28eef786fab9425
tanvir1Ahmed <tanvir.ahmed.ta.khan@gmail.com>
2025-12-23 10:41:52 +0600

    Scribble 1.0.0

--- Scribble API/Scribble API/Program.cs
+++ Scribble API/Scribble API/Program.cs
@@ -79,7 +79,8 @@ builder.Services.AddHealthChecks();
 
 // Add CORS for React frontend
 var allowedOrigins = builder.Configuration.GetSection("Cors:AllowedOrigins").Get<string[]>() 
-    ?? ["http://localhost:5173", "http://localhost:3000", "http://localhost:5174", "http://localhost:5175"];
+    ?? ["http://localhost:5173", "http://localhost:3000", "http://localhost:5174", "http://localhost:5175",
+    "https://scribble-ui.netlify.app/"];
 
 builder.Services.AddCors(options =>
 {
